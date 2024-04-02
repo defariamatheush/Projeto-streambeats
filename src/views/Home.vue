ef85d665354154c7597b867a08bcea6deb470ebd
Matheus Henrique de Faria <defaria.matheush@gmail.com>
2024-04-02 07:56:16 -0300

    Adicionado Cards dos streamers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,29 +4,7 @@
     <v-divider class="ma-5"></v-divider>
     <v-row>
       <v-col cols="12" sm="2" >
-        <v-card variant="tonal" prepend-avatar="" title="Matheus" subtitle="Jogando League of Legends">
-          <template v-slot:prepend>
-            <v-avatar color="blue-darken-2">
-              <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
-            </v-avatar>
-          </template>
-          <template v-slot:append>
-            <v-chip class="mb-5" prepend-icon="mdi-circle-medium" color="red" variant="flat" density="comfortable" size="small" label >
-              Streaming
-            </v-chip>
-          </template>
-          <v-card-item class="mb-2">
-
-            <v-card-title>
-              <h4 prepend-icon="$vuetify" class="text-body-1 text-black">Escutando: Chemical - Post Malone</h4>
-            </v-card-title>
-            <template v-slot:prepend>
-              <v-icon icon="mdi-music">
-              
-              </v-icon>
-            </template>
-          </v-card-item>
-        </v-card>
+        <CardStreamer />
       </v-col>
     </v-row>
 
@@ -36,10 +14,12 @@
 <script>
 
 //import AppBar from '@/components/principals/AppBar'
+import CardStreamer from '@/components/CardStreamer.vue';
 export default {
   name: 'HomeView',
   components: {
     //AppBar
+    CardStreamer
   }
 }
 </script>
